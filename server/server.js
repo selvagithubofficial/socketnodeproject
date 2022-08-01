ed4b5c95233715c51d1b7dc4f55b04bfd13ff212
selvakumar <selvakumarofficial1@gmail.com>
2022-08-01 17:44:05 +0530

    socket io custum event

--- server/server.js
+++ server/server.js
@@ -19,9 +19,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("A new user just connected server side");
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect',  (message) => {
         console.log("A new user just disconnected  from client");
-    })
+    });
+
+    
+
+    socket.on('createMessage', (message) => {
+        console.log(`create Message : `, message);
+        // io.emit('newMessage', {from :'murali',text:"MURALI IS ONLINE",CreatedAt: new Date() });
+         socket.broadcast.emit('newMessage', {from :'murali',text:"MURALI IS ONLINE",CreatedAt: new Date() });
+    });
+
+   
 })
 
 
