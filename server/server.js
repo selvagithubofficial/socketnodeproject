b08f9f5b361b0c32a6628f90abd1d94970dfb76c
selvakumar <selvakumarofficial1@gmail.com>
2022-08-01 16:45:41 +0530

    socket io connect disconnect implementation

--- server/server.js
+++ server/server.js
@@ -2,15 +2,31 @@ const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-
 const publicPath = path.join('__dirname','/../public');
 
 var app = express();
+
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+
+io.on('connection', (socket) => {
+    console.log("A new user just connected server side");
+
+    socket.on('disconnect', () => {
+        console.log("A new user just disconnected  from client");
+    })
+})
+
+
+server.listen(PORT, () => {
     console.log(`server is up on port ${PORT}`)
+    console.log(server);
 })
 
